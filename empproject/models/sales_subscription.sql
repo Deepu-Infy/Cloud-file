98585f2c7f10a9a1f3c8b579f0b9c24da016e6ac
Deepu_Kumar <34362824+Deepu-Infy@users.noreply.github.com>
2026-04-04 19:13:31 +0530

    Create sales_subscription.sql
    Sales_subscription

---
+++ empproject/models/sales_subscription.sql
@@ -0,0 +1,13 @@
+{% set grouping_cols = ['COUNTRY', 'CITY'] %}
+
+SELECT 
+    {% for col in grouping_cols -%}
+    {{ col }},
+    {%- endfor %}
+    count(customer_id) as subscription_count
+FROM {{ source('subscriber', 'cust_subscriber') }}
+GROUP BY 
+    {% for col in grouping_cols -%}
+    {{ loop.index }}{{ "," if not loop.last }}
+    {%- endfor %}
+ 
