6dad5ff993cf6035d8f32ba6c319ff6468cac57a
Deepu_Kumar <34362824+Deepu-Infy@users.noreply.github.com>
2026-04-06 19:10:18 +0530

    Update dbt run command to use profile 'empproject'

--- empproject/dbt_run.py
+++ empproject/dbt_run.py
@@ -6,7 +6,7 @@ dbt = dbtRunner()
 # Define the command arguments
 # Specify project and profiles directories if they are not in the current working directory
 # args = ["run", "--project-dir", "/path/to/my/dbt_project", "--profiles-dir", "/path/to/my/profiles"]
-args = ["run", "--select", "cust_subscriber2.sql", "--target", "dev"]
+args = ["run", "--select", "cust_subscriber2.sql", "--profile", "empproject", "--target", "STG"]
 
 # Invoke the command
 res: dbtRunnerResult = dbt.invoke(args)
