6fcf19a2a784420b7a05642dc789ccf592cc205d
Deepu_Kumar <34362824+Deepu-Infy@users.noreply.github.com>
2026-04-06 18:41:42 +0530

    Add profile and target options to dbt run command

--- empproject/dbt_run.py
+++ empproject/dbt_run.py
@@ -5,8 +5,7 @@ dbt = dbtRunner()
 
 # Define the command arguments
 # Specify project and profiles directories if they are not in the current working directory
-# args = ["run", "--project-dir", "/path/to/my/dbt_project", "--profiles-dir", "/path/to/my/profiles"]
-args = ["run", "--select", "cust_subscriber2.sql"]
+args = ["run", "--select", "cust_subscriber2.sql", "--profile", "empprofile", "--target", "dev"]
 
 # Invoke the command
 res: dbtRunnerResult = dbt.invoke(args)
